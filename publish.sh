59de72091fab798421c6c3fb32917d19b53cbd2d
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-07-29 18:36:23 +0100

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ test $steam_acc || { echo "No steam account given"; exit 1; }
 test -d "$build_dir" || { echo "No build; run ./create_build.sh"; exit 1; }
 test -f "$build_dir/workshopitem.vdf" || { echo "Not a steamcmd build; aborting"; exit 1; }
 
-steamcmd +login $1 +workshop_build_item $(pwd)/build/workshopitem.vdf +quit || {
+steamcmd +login "$steam_acc" +workshop_build_item $(pwd)/build/workshopitem.vdf +quit || {
     echo "Workshop publish failed";
     exit 1;
 }
