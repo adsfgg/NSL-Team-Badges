84e93d44e7d66f56fb32ac423b56912942999718
adsfgg <11364579+adsfgg@users.noreply.github.com>
2024-01-29 22:57:20 +0000

    Remove prints, fix typo

--- src/lua/shine/extensions/nsltournamentbadges.lua
+++ src/lua/shine/extensions/nsltournamentbadges.lua
@@ -13,7 +13,6 @@ function Plugin:Initialise()
         if errStr then
             Print("Error in parsing ns2panel NSL badge data: " .. ToString(errStr))
         else
-            Print("Data successfully fetched from ns2panel")
             self.badgeData = parsedData
         end
     end)
@@ -28,26 +27,19 @@ function Plugin:Cleanup()
 end
 
 Shine.Hook.Add( "ClientConnect", "ApplyNSLBadges", function ( Client )
-    Print("Client with ns2id %s connected", Client:GetUserId())
-
     for _,data in ipairs(Plugin.badgeData) do
         local players = data['players']
         local badge = data['id']
         local name = data['name']
-        Print("Checking badge %s", badge)
         for _,playerId in ipairs(players) do
-            Print("Checking playerId %s", playerId)
             if playerId == Client:GetUserId() then
-                Print("Granting %s badge %s (%s)", Client:GetUserId(), badge, name)
                 local setBadge = GiveBadge( playerId, badge, 5 )
                 if not setBadge then
-                    Print("Falied to grant badge")
                     return
                 end
 
-                local setBadgeName = SetFormatBadgeName( badge, name )
+                local setBadgeName = SetFormalBadgeName( badge, name )
                 if not setBadgeName then
-                    Print("Failed to set badge name")
                     return
                 end
                 return
